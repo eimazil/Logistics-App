ba9b95deb131bdca97a62deaa53893a52bed9d11
Eimantas Žilevičius <eimantaszilevicius@gmail.com>
2022-11-20 10:52:11 +0200

    Veikia hamburgeris

--- logistics/src/Components/Nav.jsx
+++ logistics/src/Components/Nav.jsx
@@ -6,65 +6,67 @@ function Nav({ status }) {
       <div className="row">
         <div className="col-12">
           <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-            <div className="navbar-brand">Logistics Hub</div>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav mr-auto">
-                {status === 1 ||
-                status === 2 ||
-                status === 3 ||
-                status === 4 ? (
-                  <NavLink
-                    to="/"
-                    end
-                    className={({ isActive }) =>
-                      isActive ? "nav-link active" : "nav-link"
-                    }
-                  >
-                    Information
-                  </NavLink>
-                ) : null}
-                {status === 3 ? (
-                  <NavLink
-                    to="/secondLink"
-                    className={({ isActive }) =>
-                      isActive ? "nav-link active" : "nav-link"
-                    }
-                  >
-                    Boxes
-                  </NavLink>
-                ) : null}
-                {status === 3 ? (
-                  <NavLink
-                    to="/thirdLink"
-                    className={({ isActive }) =>
-                      isActive ? "nav-link active" : "nav-link"
-                    }
-                  >
-                    Containers
-                  </NavLink>
-                ) : null}
-                {status !== 1 ? (
-                  <NavLink to="/logout" className="nav-link">
-                    Logout
-                  </NavLink>
-                ) : null}
-                {status === 1 ? (
-                  <NavLink to="/login" className="nav-link">
-                    Login
-                  </NavLink>
-                ) : null}
-              </ul>
+            <div className="container-fluid">
+              <div className="navbar-brand">Logistics Hub</div>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                  {status === 1 ||
+                  status === 2 ||
+                  status === 3 ||
+                  status === 4 ? (
+                    <NavLink
+                      to="/"
+                      end
+                      className={({ isActive }) =>
+                        isActive ? "nav-link active" : "nav-link"
+                      }
+                    >
+                      Information
+                    </NavLink>
+                  ) : null}
+                  {status === 3 ? (
+                    <NavLink
+                      to="/secondLink"
+                      className={({ isActive }) =>
+                        isActive ? "nav-link active" : "nav-link"
+                      }
+                    >
+                      Boxes
+                    </NavLink>
+                  ) : null}
+                  {status === 3 ? (
+                    <NavLink
+                      to="/thirdLink"
+                      className={({ isActive }) =>
+                        isActive ? "nav-link active" : "nav-link"
+                      }
+                    >
+                      Containers
+                    </NavLink>
+                  ) : null}
+                  {status !== 1 ? (
+                    <NavLink to="/logout" className="nav-link">
+                      Logout
+                    </NavLink>
+                  ) : null}
+                  {status === 1 ? (
+                    <NavLink to="/login" className="nav-link">
+                      Login
+                    </NavLink>
+                  ) : null}
+                </ul>
+              </div>
             </div>
           </nav>
         </div>
